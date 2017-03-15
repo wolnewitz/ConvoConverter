11c3c720cb14579b915b0ed001d72362d297b5df
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-15 10:41:19 -0700

    Remove log

--- src/replaceName.js
+++ src/replaceName.js
@@ -3,7 +3,6 @@ import getNameRegex from './getNameRegex';
 const replaceName = (convo, originalText, replaceText) => {
   if (!convo.length) { return ''; }
   const nameRegex = getNameRegex(originalText);
-  console.log('nameRegex', nameRegex)
   const replaced = convo.replace(nameRegex, `${replaceText}`);
   return replaced;
 }
